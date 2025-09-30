a5f8f486bc2c443192b4742a9066c1792340fa97
Anastasia Egorova <air-neba@yandex.ru>
2025-09-30 15:42:14 +0300

    в компоненте app.tsx сделана настройка по роуту / и /feed

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,20 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <Router>
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<ConstructorPage />} /> 
+        <Route path="/feed" element={<Feed />} />  
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
